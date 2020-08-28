416c86fc2748a2f9e3269ab8a20d069c84448ee4
Luis Cespedes <ljcespedes27@gmail.com>
2020-08-27 22:54:05 -0400

    Setting up React and my folder structure

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+
+
+const layout =(props) =>(
+
+    <Aux>
+        <div>
+            toolbar,sidebar,backdrop
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+
+export default layout; 
